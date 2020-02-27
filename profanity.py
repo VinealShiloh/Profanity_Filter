b86c6acd95124e6645b35d01251096d525221114
Rude:P <33525627+VinealShiloh@users.noreply.github.com>
2020-02-27 20:39:07 +0530

    Update profanity.py

--- profanity.py
+++ profanity.py
@@ -6,21 +6,21 @@ class Profanity_Filter():
 
 		pf = ProfanityFilter()
 
-		#Opens the text file from the given location
-		file_location = open('/home/vneal/Frs_prj/badwords.txt')
+		#Opens the text file from the given location.
+		file_location = open('/yourfilelocation/filename.txt')
 
-		# Opens the file. This line should be added when if your text file is in same location a program 
+		# Opens the file. This line should be added when if your text file is in same location a program.
 		#file_location = open('profanity.txt')
 		
-		#Read is a built in function of python to read files
+		#Read is a built in function of python to read files.
 		content_of_file = file_location.read()
 		
-		#Censor is a built in function of ProfanityFilter package to check profanity of a sentence
+		#Censor is a built in function of ProfanityFilter package to check profanity of a sentence.
 		text = pf.censor(content_of_file)
 		
-		#Prints the contents of the file where offensive words are marked by "*"
+		#Prints the contents of the file where offensive words are marked by "*".
 		print(text)
 
-# Main function
+# Main function.
 if __name__ == '__main__':
 	Profanity_Filter.check_profanity_filter_text()
