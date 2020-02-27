a5790c826fd2295663998927a3fd19fa6c10924d
Rude:P <33525627+VinealShiloh@users.noreply.github.com>
2020-02-27 21:30:16 +0530

    Update profanity_checker.py

--- profanity_checker.py
+++ profanity_checker.py
@@ -2,12 +2,16 @@ from profanity_check import predict, predict_prob
 
 file_location = open (Random.txt)
 
+#Reads the file 
 content_of_file = file_location.read()
 
+#Finds the profanity of the text file
 print(predict([content_of_file]))
 
+#Finds the profanity of a file
+print(predict_prob([content_of_file]))
 
-print(predict_prob(['hi guys im a bad boy','shutup you kid',
-'welcome to python programming',
-'checking profanity of the text file']))
+#Test line to find the profanity of a sentence
+#print(predict_prob(['hi guys im a bad boy','shutup you kid','welcome to python programming','checking profanity of the text file']))
 
+file_location.close()
